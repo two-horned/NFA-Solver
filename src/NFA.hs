fefc200c029b3417a8c6248fc3df9a5f9d496c21
Said Kadrioski <said@kadrioski.de>
2025-05-29 02:19:25 +0200

    Remove counter example. Simplify algorithm (no performance benefit).

--- src/NFA.hs
+++ src/NFA.hs
@@ -11,38 +11,18 @@ type NFA a =
     S.IntSet -- Final States
   )
 
--- | Calculate a word in the NFA the naive way (exponential runtime).
---   Not used, just written down for comparison.
-naiveCalcNFA :: (Hashable a) => NFA a -> [a] -> Bool
-naiveCalcNFA nfa word = go word i S.empty
-  where
-    (eps, i, trs, fin) = nfa
-    look k = M.findWithDefault S.empty k trs
-    go [] s fs =
-      let epi = look (s, eps) S.\\ fs
-          bad = [go [] n (S.insert n fs) | n <- S.toList epi]
-       in S.member s fin || or bad
-    go ws@(w : ws') s fs =
-      let gud = [go ws' n S.empty | n <- S.toList (look (s, w))]
-          epi = look (s, eps) S.\\ fs
-          bad = [go ws n (S.insert n fs) | n <- S.toList epi]
-       in or $ gud ++ bad
-
 -- | Calculate a word using Said's NFA-Algorithm.
 --   Runtime is linear regarding the word length,
 --   and polynomial regarding the amount of states.
 calcNFA :: (Hashable a) => NFA a -> [a] -> Bool
-calcNFA nfa word = go word (S.singleton i)
+calcNFA nfa = cleanup . go
   where
     (eps, i, trs, fin) = nfa
-    look k = M.findWithDefault S.empty k trs
-    concatMapS f = S.foldl (\b a -> S.union (f a) b) S.empty
-    nextgen fs w s =
-      let leg = look (s, w)
-          gel = look (s, eps)
-          fs' = S.insert s fs
-          pro = concatMapS (nextgen fs' w) $ gel S.\\ fs
-       in S.union leg pro
-    go [] sts =
-      not $ S.disjoint fin (S.union sts (concatMapS (nextgen S.empty eps) sts))
-    go (w : ws) sts = go ws (concatMapS (nextgen S.empty w) sts)
+    look w s = M.findWithDefault S.empty (s, w) trs
+    sfoldMap f = S.foldl' (\b a -> f a <> b) mempty -- remove when container is v0.8
+    kids w = sfoldMap (look w)
+    growgen gs = let ns = flip (S.\\) <*> kids eps $ gs
+        in if S.null ns then gs else growgen (gs <> ns)
+    nextgen w = kids w . growgen
+    cleanup = not . S.disjoint fin . growgen
+    go = foldl' (flip nextgen) (S.singleton i)
